e948080a45a899e19253f2c8140d3881b0b48f86
Mark Wylde <me@markwylde.com>
2020-04-16 23:05:29 +1000

    Lint and add binding to all attributes

--- demo/index.js
+++ demo/index.js
@@ -1,18 +1,19 @@
-const products = []
+const products = [];
 for (let i = 0; i < 100; i++) {
   products.push({
     title: 'test ' + i,
     tags: ['blue', i % 2 ? 'odd' : 'even'],
+    complete: !(i % 5),
     price: i
-  })
+  });
 }
 
 const state = window.state = {
   name: 'testing testing 123',
   products,
   totalPrice: products.reduce((total, item) => total + item.price, 0)
-}
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  fastner(document.body, state)
-})
+  fastner(document.body, state);
+});
--- index.js
+++ index.js
@@ -1,63 +1,71 @@
-const {fastn, binding, mutate} = require('./fastn')
+const { fastn, binding, mutate } = require('./fastn');
 
 function fetchAttributes (node) {
-  return node && Array.prototype.reduce.call(node.attributes, function(list, attribute) {
-    list[attribute.name] = attribute.value;
+  if (!node) {
+    return {};
+  }
+
+  return Array.prototype.reduce.call(node.attributes, function (list, attribute) {
+    if (attribute.value.startsWith('binding(')) {
+      list[attribute.name] = eval(attribute.value);
+    } else {
+      list[attribute.name] = attribute.value;
+    }
+
     return list;
-  }, {}) || {};
-};
+  }, {});
+}
 
 function createChild (child, children) {
-  const attributes = fetchAttributes(child)
+  const attributes = fetchAttributes(child);
   if (attributes.list) {
-    attributes.items = eval(attributes.list)
+    attributes.items = eval(attributes.list);
 
     attributes.template = () =>
-      objectToFastn(Array.from(child.childNodes).find(el => el.nodeName !== '#text'))
+      objectToFastn(Array.from(child.childNodes).find(el => el.nodeName !== '#text'));
 
-    return fastn('list:' + child.tagName, attributes)
+    return fastn('list:' + child.tagName, attributes);
   }
 
   if (child.tagName === 'text') {
     return fastn('text', {
       text: eval(attributes.data)
-    })
+    });
   }
-  
-  return fastn(child.tagName, attributes, ...children)
 
+  return fastn(child.tagName, attributes, ...children);
 }
 
 function objectToFastn (obj) {
   const children = Array.from(obj.childNodes).map(child => {
     if (child.nodeName === '#text') {
-      return fastn('text', {text: child.data})
+      return fastn('text', { text: child.data });
     }
 
     if (child.childNodes.length > 0) {
-      return objectToFastn(child)
+      return objectToFastn(child);
     }
 
-    return createChild(child, [child.textContent])
-  })
+    return createChild(child, [child.textContent]);
+  });
 
-  return createChild(obj, children)
+  return createChild(obj, children);
 }
 
 function fastner (element, state) {
   const dom = (new DOMParser()).parseFromString(
     `<${element.tagName}>${element.innerHTML}</${element.tagName}>`
-  , 'text/xml')
+    , 'text/xml');
 
   const app = objectToFastn(dom.childNodes[0])
-    .attach(state)
+    .attach(state);
 
-  app.render()
+  app.render();
 
-  element.replaceWith(app.element)
+  element.replaceWith(app.element);
 }
 
-window.fastn = fastn
-window.mutate = mutate
-window.binding = binding
-window.fastner = fastner
+window.fastn = fastn;
+window.mutate = mutate;
+window.binding = binding;
+window.fastner = fastner;
